0d382087c0eebf30cdffc15f32e8b75b2de43b15
Kevin Qi <qiwen@qiwen.name>
2020-08-30 20:31:57 +0800

    support GPU benchmark

--- pytorch_ext/lltm/benckmark.py
+++ pytorch_ext/lltm/benckmark.py
@@ -1,34 +1,41 @@
 import time
 import torch
 from cpp.lltm import LLTM as cpp_lltm
-from py.lltm import LLTM as py_lltm
+from py import LLTM as py_lltm
 
 
-def benckmark(lstm_func, description, steps=1000):
+def benckmark(lstm_func, description, device='cpu', steps=1000):
     batch_size = 16
     input_features = 32
     state_size = 128
+    torch_device = torch.device(device)
 
-    X = torch.randn(batch_size, input_features)
-    h = torch.randn(batch_size, state_size)
-    C = torch.randn(batch_size, state_size)
+    X = torch.randn(batch_size, input_features, device=torch_device)
+    h = torch.randn(batch_size, state_size, device=torch_device)
+    C = torch.randn(batch_size, state_size, device=torch_device)
 
-    rnn = lstm_func(input_features, state_size)
+    rnn = lstm_func(input_features, state_size).to(torch_device)
 
     forward = 0
     backward = 0
     for _ in range(steps):
         start = time.time()
         new_h, new_C = rnn(X, (h, C))
+        if device == 'cuda':
+            torch.cuda.synchronize()
         forward += time.time() - start
 
         start = time.time()
         (new_h.sum() + new_C.sum()).backward()
+        if device == 'cuda':
+            torch.cuda.synchronize()
         backward += time.time() - start
 
-    print('{}, Forward: {:.3f} s | Backward {:.3f} s'.format(
-        description, forward, backward))
+    print('{}, Device: {}, Forward: {:.3f} s | Backward {:.3f} s'.format(
+        description, device, forward, backward))
 
+benckmark(cpp_lltm, 'C++', device='cuda', steps=10000)
+benckmark(py_lltm, 'Py ', device='cuda', steps=10000)
 
-benckmark(cpp_lltm, 'C++', steps=10000)
-benckmark(py_lltm, 'Py ', steps=10000)
+benckmark(cpp_lltm, 'C++', device='cpu', steps=10000)
+benckmark(py_lltm, 'Py ', device='cpu', steps=10000)
