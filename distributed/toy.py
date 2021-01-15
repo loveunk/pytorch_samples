a171439421d647126e868355e48337e52dbc8af7
Kevin <qiwen@qiwen.name>
2021-01-15 23:26:18 +0800

    Update toy.py

--- distributed/toy.py
+++ distributed/toy.py
@@ -5,7 +5,7 @@ from time import sleep
 from random import randint
 
 def foo(rank, world_size):
-    for step in range(100):
+    for step in range(20):
         # get random int
         value = randint(0, 10)
         # group all ranks
@@ -14,15 +14,20 @@ def foo(rank, world_size):
         # compute reduced sum
         tensor = torch.IntTensor([value])
         dist.all_reduce(tensor, op=dist.reduce_op.SUM, group=group)
-        print('rank: {}, step: {}, value: {}, reduced sum: {}.'.format(rank,step,value,float(tensor)))
+        print('rank: {}, step: {}, value: {}, reduced sum: {}.'.format(
+              rank,step,value,float(tensor)))
         sleep(1)
 
-def initialize(rank, world_size, ip, port):
-    dist.init_process_group(backend='tcp',init_method='tcp://{}:{}'.format(ip, port),rank=rank,world_size=world_size)
+def initialize(backend, rank, world_size, ip, port):
+    dist.init_process_group(backend=backend,
+                            init_method='tcp://{}:{}'.format(ip, port),
+                            rank=rank,
+                            world_size=world_size)
     foo(rank, world_size)
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--backend', type=str, default='gloo')
     parser.add_argument('--ip', type=str, default='127.0.0.1')
     parser.add_argument('--port', type=str, default='20000')
     parser.add_argument('--rank', '-r', type=int)
@@ -31,5 +36,9 @@ def main():
     print(args)
     initialize(args.rank, args.world_size, args.ip, args.port)
 
+
+'''
+python toy.py --rank ${DLWS_ROLE_IDX} --world-size ${DLWS_WORKER_NUM} --ip ${DLWS_SD_worker0_IP} --port 22000
+'''
 if __name__ == '__main__':
     main()
