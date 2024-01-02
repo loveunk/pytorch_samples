feebfc519bb7becd6f4240b81035cb8843d7d5e1
Kevin <qiwen@qiwen.name>
2024-01-02 15:17:40 +0800

    Update ds_demo.py

--- distributed/deepspeed/ds_demo.py
+++ distributed/deepspeed/ds_demo.py
@@ -37,41 +37,6 @@ class Net(nn.Module):
         return output
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-
-
-def test(args, model, device, test_loader, epoch):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-    accuracy = correct / len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset), 100. * accuracy))
-
-
-
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
@@ -151,5 +116,7 @@ if __name__ == '__main__':
     main()
 
 
-
+# start training with deepspeed:
 # NCCL_P2P_DISABLE=1 deepspeed ds_demo.py --deepspeed --deepspeed_config "$PWD/ds_config.json"
+
+# reference: https://github.com/OvJat/DeepSpeedTutorial/tree/main
